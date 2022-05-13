7a01548eb88d1f2ec75d8cbfe175db324f720c48
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2022-05-13 17:19:37 -0300

    memos update

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-full p-8 flex justify-center items-center space-x-10">
+      <div className="w-full p-8 flex justify-center items-center space-x-10 bg-stone-800">
         <button
           className={`text-orange-200 hover:text-orange-400 ${
             currentPage === 0 && "border-b-2 border-orange-200"
@@ -135,172 +135,171 @@ const Home: NextPage = () => {
         )}
         <ConnectButton />
       </div>
-      <main className="text-white text-[30px] w-[740px] text-center">
+      <main className="text-white text-[30px] text-center">
         <div className="my-20 font-bold text-[64px]">
           {currentPage === 0 ? "Buy me a coffee" : "Memos received"}
         </div>
-        <div className="flex flex-row justify-between items-center">
-          {currentPage === 0 ? (
-            <>
-              <div className="flex flex-col justify-center items-center bg-stone-700  rounded-[38px] shadow-xl w-[400px] h-[400px]">
-                {loading && (
-                  <>
-                    <Image
-                      src="/images/coffee.svg"
-                      alt="coffee"
-                      width={200}
-                      height={200}
-                      className="animate-spin"
-                    />
-                  </>
-                )}
-                {!loading && Object.keys(selected).length === 0 && (
-                  <>
-                    <Image
-                      src="/images/coffee.svg"
-                      alt="coffee"
-                      width={200}
-                      height={200}
-                      className="hover:animate-spin"
-                    />
-                  </>
-                )}
-                {!loading && Object.keys(selected).length > 0 && (
-                  <div className="space-y-4 text-sm w-full p-10">
-                    <Image
-                      src="/images/coffee.svg"
-                      alt="coffee"
-                      width={80}
-                      height={80}
-                      className="hover:animate-spin"
-                    />
 
-                    <div className="p-4 bg-stone-900 hover:bg-stone-800 active:bg-stone-800 rounded-xl">
-                      <input
-                        type="text"
-                        spellCheck={false}
-                        className="bg-transparent outline-none w-full"
-                        placeholder="Type your name"
-                        value={name}
-                        onChange={handleNameChange}
-                      />
-                    </div>
-                    <div className="p-4 bg-stone-900 hover:bg-stone-800 active:bg-stone-800 rounded-xl">
-                      <textarea
-                        spellCheck={false}
-                        style={{ resize: "none" }}
-                        className="bg-transparent outline-none w-full"
-                        placeholder="Fill a message"
-                        value={message}
-                        onChange={handleMessageChange}
-                      />
-                    </div>
+        {currentPage === 0 ? (
+          <div className="flex flex-row justify-between items-center mb-[100px]">
+            <div className="flex flex-col justify-center items-center bg-gradient-to-t from-stone-600 to-stone-800 rounded-[38px] shadow-2xl w-[400px] h-[400px]">
+              {loading && (
+                <>
+                  <Image
+                    src="/images/coffee.svg"
+                    alt="coffee"
+                    width={200}
+                    height={200}
+                    className="animate-spin"
+                  />
+                </>
+              )}
+              {!loading && Object.keys(selected).length === 0 && (
+                <>
+                  <Image
+                    src="/images/coffee.svg"
+                    alt="coffee"
+                    width={200}
+                    height={200}
+                    className="hover:animate-spin"
+                  />
+                </>
+              )}
+              {!loading && Object.keys(selected).length > 0 && (
+                <div className="space-y-4 text-sm w-full p-10">
+                  <Image
+                    src="/images/coffee.svg"
+                    alt="coffee"
+                    width={80}
+                    height={80}
+                    className="hover:animate-spin"
+                  />
 
-                    <button
-                      disabled={!data}
-                      className={`flex justify-center items-center ${
-                        data
-                          ? "bg-green-700 hover:bg-green-600 active:bg-green-500"
-                          : "bg-green-700 opacity-50"
-                      }  p-4 rounded-3xl w-full shadow-xl`}
-                      onClick={handleBuy}
-                    >
-                      Send a {selected.name} Coffee (
-                      {selected?.value?.toString()} ETH)
-                    </button>
-
-                    <button
-                      className="flex justify-center items-center w-full text-stone-900 hover:text-stone-800"
-                      onClick={() => setSelected({} as CoffeeType)}
-                    >
-                      Cancel
-                    </button>
+                  <div className="p-4 bg-stone-900 hover:bg-stone-800 active:bg-stone-800 rounded-xl">
+                    <input
+                      type="text"
+                      spellCheck={false}
+                      className="bg-transparent outline-none w-full"
+                      placeholder="Type your name"
+                      value={name}
+                      onChange={handleNameChange}
+                    />
                   </div>
-                )}
-              </div>
+                  <div className="p-4 bg-stone-900 hover:bg-stone-800 active:bg-stone-800 rounded-xl">
+                    <textarea
+                      spellCheck={false}
+                      style={{ resize: "none" }}
+                      className="bg-transparent outline-none w-full"
+                      placeholder="Fill a message"
+                      value={message}
+                      onChange={handleMessageChange}
+                    />
+                  </div>
 
-              <div className="flex flex-col justify-center space-y-10">
-                {coffeeTypes.map((coffee, index) => (
-                  <div
-                    className="flex flex-row items-center space-x-5"
-                    key={index}
+                  <button
+                    disabled={!data}
+                    className={`flex justify-center items-center ${
+                      data
+                        ? "bg-green-700 hover:bg-green-600 active:bg-green-500"
+                        : "bg-green-700 opacity-50"
+                    }  p-4 rounded-3xl w-full shadow-xl`}
+                    onClick={handleBuy}
                   >
-                    <button
-                      className="active:scale-90"
-                      onClick={() => handleSelect(coffee)}
-                    >
-                      <div className="relative flex justify-center items-center">
+                    Send a {selected.name} Coffee ({selected?.value?.toString()}{" "}
+                    ETH)
+                  </button>
+
+                  <button
+                    className="flex justify-center items-center w-full text-stone-900 hover:text-stone-800"
+                    onClick={() => setSelected({} as CoffeeType)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              )}
+            </div>
+
+            <div className="flex flex-col justify-center space-y-10 ml-12">
+              {coffeeTypes.map((coffee, index) => (
+                <div
+                  className="flex flex-row items-center space-x-5"
+                  key={index}
+                >
+                  <button
+                    className="active:scale-90"
+                    onClick={() => handleSelect(coffee)}
+                  >
+                    <div className="relative flex justify-center items-center">
+                      <Image
+                        src={
+                          selected === coffee
+                            ? "/images/box-highlight.svg"
+                            : "/images/box.svg"
+                        }
+                        alt="box"
+                        width={100}
+                        height={100}
+                      />
+                      <div
+                        className={`absolute ${
+                          coffee?.class
+                        } hover:animate-bounce ${
+                          selected === coffee ? "opacity-100" : "opacity-50"
+                        }`}
+                      >
                         <Image
-                          src={
-                            selected === coffee
-                              ? "/images/box-highlight.svg"
-                              : "/images/box.svg"
-                          }
-                          alt="box"
-                          width={100}
-                          height={100}
+                          src="/images/cup.svg"
+                          alt="cup"
+                          width={coffee?.boxSize}
+                          height={coffee?.boxSize}
                         />
-                        <div
-                          className={`absolute ${
-                            coffee?.class
-                          } hover:animate-bounce ${
-                            selected === coffee ? "opacity-100" : "opacity-50"
-                          }`}
-                        >
-                          <Image
-                            src="/images/cup.svg"
-                            alt="cup"
-                            width={coffee?.boxSize}
-                            height={coffee?.boxSize}
-                          />
-                        </div>
-                      </div>
-                    </button>
-                    <div className="text-left">
-                      <div>{coffee.name}</div>
-                      <div className="text-[16px]">
-                        {coffee?.value?.toString()} ETH
                       </div>
                     </div>
+                  </button>
+                  <div className="text-left">
+                    <div>{coffee.name}</div>
+                    <div className="text-[16px]">
+                      {coffee?.value?.toString()} ETH
+                    </div>
                   </div>
-                ))}
-              </div>
-            </>
-          ) : (
-            <div className="flex justify-center items-center w-full flex-wrap">
-              {memos.map((memo, index) => (
-                <div
-                  key={index}
-                  className="bg-black p-6 rounded-2xl text-left text-sm space-y-2 shadow-xl m-4"
-                >
-                  <div>
-                    <span className="text-orange-300 text-[18px]">
-                      {memo[2]}
-                    </span>{" "}
-                    sent a{" "}
-                    <span className="border-b-2">{handleSize(memo[4])}</span>{" "}
-                    coffee for
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center w-full flex-wrap w-[800px]">
+            {memos.map((memo, index) => (
+              <div
+                key={index}
+                className="bg-stone-800 p-6 rounded-2xl text-left text-sm space-y-2 shadow-2xl m-4"
+              >
+                <div>
+                  <div className="text-orange-300 text-[22px]">{memo[2]}</div>
+                  <div className="leading-8">
+                    He sent a {handleSize(memo[4])} coffee
                     <span className="text-[12px] ml-1">
+                      (
                       {Number(memo[4]) === 1
                         ? "0.001 ETH"
                         : Number(memo[4]) === 2
                         ? "0.003 ETH"
                         : "0.005 ETH"}
+                      )
                     </span>
                   </div>
-
-                  <div className="text-[12px] text-orange-300">
-                    Message:
-                    <div className="text-white">{memo[3]}</div>
-                  </div>
-                  <div className="text-[12px] text-orange-300">
-                    Wallet:<div className="text-white">{memo[0]}</div>
-                  </div>
                 </div>
-              ))}
-            </div>
-          )}
-        </div>
+
+                <div className="text-[12px] text-orange-300">
+                  Message:
+                  <div className="text-white">{memo[3]}</div>
+                </div>
+                <div className="text-[12px] text-orange-300">
+                  Wallet:<div className="text-white">{memo[0]}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </main>
 
       <ToastContainer
@@ -316,7 +315,7 @@ const Home: NextPage = () => {
         pauseOnHover
         theme="dark"
       />
-      <footer className="fixed bottom-0 p-10">
+      <footer className="p-10">
         <div className="text-white">Skeletor Ⓒ 2022</div>
       </footer>
     </div>
